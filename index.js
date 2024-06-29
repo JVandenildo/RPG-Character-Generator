da7d9db984671340178cf0827f00fd7fec4d181d
J. Vandenildo <jvandenildo@gmail.com>
2024-06-29 19:39:56 -0300

    minor updates

--- index.js
+++ index.js
@@ -1,37 +1,64 @@
 const charNames = [
+	"Alambique",
 	"Bolo-fofo",
 	"Cascavel",
 	"Calabreso",
 	"CNPJoto",
 	"Descascado",
 	"Euclides",
+	"Figaro",
+	"Gul",
+	"Huai",
+	"Ihain",
 	"Jezebel",
+	"Karamba",
 	"Laravel",
+	"Merimbal",
+	"Natuza",
+	"Obtuso",
 	"Pegmatito",
+	"Queue",
 	"Rochoso",
 	"Rogéria",
+	"Sucuri",
+	"Tuta",
+	"Uiui",
+	"Vidala",
+	"Ximbica",
+	"Will",
+	"Ysadoro",
+	"Zigzago",
 ];
 const charRacesM = ["Anão", "Elfo", "Gnomo", "Humano", "Orc"];
-const charRacesF = ["Anã", "Elfa", "Gnoma", "Humana", "Orca"];
+const charRacesF = ["Anã", "Elfa", "Gnoma", "Humana", "Orc"];
 const charClassesM = ["Arqueiro", "Feiticeiro", "Guerreiro", "Ladino", "Monge"];
-const charClassesF = ["Arqueira", "Feiticeira", "Guerreira", "Ladina", "Monge"];
+const charClassesF = ["Arqueira", "Feiticeira", "Guerreira", "Ladina", "Monja"];
 
 class Character {
 	/**
 	 * @param {string} inputRace will receive the user input for character's name, otherwise a random value from charName
 	 * @param {string} inputClass receives random value from charClasses
 	 */
-	constructor(inputRace, inputClass) {
+	constructor() {
 		this._race = this.getRace;
 		this._class = this.getClass;
 		this._name = this.getName;
 	}
 
+	/**
+	 * @returns {string}
+	 */
 	get getName() {
 		return charNames[handleRandom(charNames)];
 	}
+	/**
+	 * @param {string} string
+	 */
 	set getName(string) {}
 
+	/**
+	 * @returns {string}
+	 */
 	get getRace() {
 		if (document.getElementById("ratioMAL").checked) {
 			return charRacesM[handleRandom(charRacesM)];
@@ -39,8 +66,14 @@ class Character {
 			return charRacesF[handleRandom(charRacesF)];
 		}
 	}
+	/**
+	 * @param {string} string
+	 */
 	set getRace(string) {}
 
+	/**
+	 * @returns {string}
+	 */
 	get getClass() {
 		if (document.getElementById("ratioMAL").checked) {
 			return charClassesM[handleRandom(charClassesM)];
@@ -48,11 +81,14 @@ class Character {
 			return charClassesF[handleRandom(charClassesF)];
 		}
 	}
+	/**
+	 * @param {string} string
+	 */
 	set getClass(string) {}
 }
 
 const charRESULT = document.querySelector("#charRESULT");
-const userCHAR = new Character("Wellington", "Warrior");
+const userCHAR = new Character(null, null);
 const btnCHAR = document.querySelector("#btnCHAR");
 const userNPT = document.querySelector("#userNPT");
 btnCHAR.addEventListener("click", showNAME);
@@ -91,9 +127,8 @@ function handleNPT(character) {
  */
 function handleRandom(range) {
 	const date = new Date();
-	const randomNumber = Math.floor(Math.random(date) * range.length);
 
-	return randomNumber;
+	return Math.floor(Math.random(date) * range.length);
 }
 
 /**
